d7fe40962d4f77bd4332bfbdb8ff28a80e6dabf7
Malcoci Marius <mariusmalcoci5@gmail.com>
2024-06-25 11:05:45 +0300

    first part of front end

---
+++ components/nav.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="nav">
+        <div class="imgCont">
+            <a href="">
+                <img src="https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/logos/Global-Header_MCCB-Logo_300x51.svg" alt="">
+
+            </a>
+
+        </div>
+        
+        <div class="navLinks">
+            <a href="">GAMES</a>
+            <a href="">SHOP</a>
+            <a href="">COMMUNITY</a>
+            <a href="">SUPPORT</a>
+        </div>
+
+        <div class="account">
+            <a href="">
+                <img src="https://cdn-icons-png.flaticon.com/128/1828/1828415.png" alt="">
+                <span>ACCOUNT</span>
+            </a>
+
+        </div>
+
+    </div>
+</body>
+</html>
